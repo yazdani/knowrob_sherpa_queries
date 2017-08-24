b35f721be25e0339b0c94421dcd8f98f00197b56
Fereshta Yazdani <yazdani@uni-bremen.de>
2017-08-24 18:36:00 +0200

    fixed bug

--- knowrob_sherpa_queries/src/main/java/com/github/knowrob_sherpa_queries/VisualizationMarker.java
+++ knowrob_sherpa_queries/src/main/java/com/github/knowrob_sherpa_queries/VisualizationMarker.java
@@ -1422,9 +1422,9 @@ public class VisualizationMarker extends AbstractNodeMain{
     public float[] transformToPose(float[] transform)
     {
 	float[] arr = new float[3];
-	arr[0] = transforms[3];
-	arr[1] = transforms[7];
-	arr[2] = transforms[11];
+	arr[0] = transform[3];
+	arr[1] = transform[7];
+	arr[2] = transform[11];
 	
 	return arr;
     }
